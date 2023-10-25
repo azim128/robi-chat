8f82829d37206499a32b6ccf63cddd06723d6230
azim128 <azimruet28@gmail.com>
2023-10-25 09:41:50 +0600

    chaterror

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import React from 'react';
 import useSpeechToText from 'react-hook-speech-to-text';
-import Chatbox from './components/chatbox';
+import Chatbox from './components/Chatbox';
+
 
 export default function App() {
-  const {
-    error,
-    interimResult,
-    isRecording,
-    results,
-    startSpeechToText,
-    stopSpeechToText,
-  } = useSpeechToText({
-    continuous: true,
-    useLegacyResults: false,
-    speechRecognitionProperties: {
-      lang: 'bn-BN',}
-  });
-
-  if (error) return <p>Web Speech API is not available in this browser 🤷‍</p>;
+ 
 
   return (
     <div>
-      <h1>Recording: {isRecording.toString()}</h1>
+      
       <Chatbox/>
-      <button onClick={isRecording ? stopSpeechToText : startSpeechToText}>
-        {isRecording ? 'Stop Recording' : 'Start Recording'}
-      </button>
-      <ul>
-        {results.map((result) => (
-          <li key={result.timestamp}>{result.transcript}</li>
-        ))}
-        {interimResult && <li>{interimResult}</li>}
-      </ul>
+      
     </div>
   );
 }
